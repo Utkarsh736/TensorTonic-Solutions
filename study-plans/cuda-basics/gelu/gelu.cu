6c6e3360800e8112f3363f7d023c501b44e014ff
Utkarsh <54865380+Utkarsh736@users.noreply.github.com>
2026-09-10 15:32:22 +0530

    Solve gelu

--- study-plans/cuda-basics/gelu/gelu.cu
+++ study-plans/cuda-basics/gelu/gelu.cu
@@ -5,9 +5,8 @@ __global__ void gelu_kernel(const float* input, float* output, int N) {
     int idx = blockIdx.x*blockDim.x + threadIdx.x;
     if(idx<N){
         float sqr_rt = sqrtf(2.0);
-        float val = erff(input[idx]/sqr_rt);
 
-        output[idx] = 0.5*input[idx]*(1 + val);
+        output[idx] = 0.5*input[idx]*(1 + erff(input[idx]/sqr_rt));
     }
 }
 
