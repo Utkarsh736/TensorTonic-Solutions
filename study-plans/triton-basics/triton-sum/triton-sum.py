687e2c5c194b43522ecea81277b78254d6c77daa
Utkarsh <54865380+Utkarsh736@users.noreply.github.com>
2026-09-10 11:52:47 +0530

    Solve triton-sum

---
+++ study-plans/triton-basics/triton-sum/triton-sum.py
@@ -0,0 +1,27 @@
+import torch
+import triton
+import triton.language as tl
+
+
+@triton.jit
+def sum_kernel(x_ptr, out_ptr, n, BLOCK_SIZE: tl.constexpr):
+    pid = tl.program_id(axis=0)
+    block_start = pid*BLOCK_SIZE
+    offs = block_start + tl.arange(0, BLOCK_SIZE)
+
+    mask = offs<n
+
+    x = tl.load(x_ptr+offs, mask=mask, other=0.0)
+
+    blk_sum = tl.sum(x, axis=0)
+
+    tl.atomic_add(out_ptr, blk_sum)
+
+
+def solve(x: torch.Tensor, out: torch.Tensor) -> None:
+    """Launch sum_kernel on the provided tensors."""
+    n = x.numel()
+    out.zero_()
+    BLOCK_SIZE = 1024
+    grid = ((n + BLOCK_SIZE - 1) // BLOCK_SIZE,)
+    sum_kernel[grid](x, out, n, BLOCK_SIZE=BLOCK_SIZE)
